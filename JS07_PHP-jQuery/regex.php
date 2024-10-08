cacb2e8e41034ab4608a7e3f776f242f1a076983
Ahmad Hayyin <baihakikiki784@gmail.com>
2024-10-08 09:09:53 +0700

    Practical Section 5 : The Use of Regex in PHP

--- JS07_PHP-jQuery/regex.php
+++ JS07_PHP-jQuery/regex.php
@@ -1,9 +1,19 @@
 <?php
-$pattern = '/[a-z]/'; // Cocokkan huruf kecil.
+// Cocokkan huruf kecil
+$pattern = '/[a-z]/';
 $text = 'This is a Sample Text.';
 if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
+    echo "Huruf kecil ditemukan!<br>";
 } else {
-    echo "Tidak ada huruf kecil!";
+    echo "Tidak ada huruf kecil!<br>";
+}
+
+// Cocokkan satu atau lebih digit
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
 }
 ?>
